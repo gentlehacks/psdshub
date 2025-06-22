967b43d8aa2b23af0879c0581357d76418ec1645
gentlehacks <salihuadamu4678@gmail.com>
2025-06-22 02:35:58 +0100

    fixind build errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 import {motion, AnimatePresence} from "framer-motion";
 import {usePathname} from "next/navigation"
-import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
